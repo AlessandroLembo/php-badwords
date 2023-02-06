5f1fd7ece9fadcd40e9414691cc03bcc6cb228a4
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-06 15:36:54 +0100

    add file word to style page

--- badwords.php
+++ badwords.php
@@ -10,10 +10,15 @@ $word = $_GET['word'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad words results</title>
+
+    <link rel="stylesheet" href="word.css">
 </head>
 <body>
-    <p><? echo $paragraph ?></p>
-    <h2><? echo $word ?></h2>
+    <div class="container">
+         <p><? echo $paragraph ?></p>
+         <h2><? echo $word ?></h2>
+    </div>
+    
     
 </body>
 </html>
